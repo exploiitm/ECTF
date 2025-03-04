08bfd022be68f041ba58bf799fdb68397212c447
InnocentZero <cs22b010@smail.iitm.ac.in>
2025-03-05 00:23:47 +0530

    cleaned up repository, ensured build works btw

--- decoder/exec.sh
+++
@@ -1,6 +0,0 @@
-make DECODER_ID=0xdeadbeef
-python -m remote.upload 2
-sleep 3
-python3 -m ectf25.utils.flash ./build/max78000.bin /dev/ttyACM2
-sleep 3
-python3 -m ectf25.tv.subscribe ../testsubnew.bin /dev/ttyACM2
